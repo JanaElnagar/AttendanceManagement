afc4086649d9285678732a0958fee0346129406b
JanaElnagar <janaelnagar18@gmail.com>
2025-12-01 12:54:34 +0200

    DoctorApproval functionality fixed

--- src/AttendanceManagement.EntityFrameworkCore/Migrations/20251201104628_MakeApproverEmployeeIdNullable.cs
+++ src/AttendanceManagement.EntityFrameworkCore/Migrations/20251201104628_MakeApproverEmployeeIdNullable.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
@@ -35,4 +35,3 @@ namespace AttendanceManagement.Migrations
         }
     }
 }
-
